093f5e6f7a1a518f3d1c324705d9fa5baab90326
billalr <billalrahimi@hotmail.com>
2022-07-31 20:54:10 -0700

    Added vector multiplication into the user defined vector math library

---
+++ include/vectorMath.hpp
@@ -0,0 +1,7 @@
+// Includes
+#include <Arduino.h>
+#include <vector>
+#include <math.h>
+
+// Function prototypes
+void VecMult(std::vector<std::vector<float> > VecA, std::vector<std::vector<float> > VecB, std::vector<std::vector<float> > &result);
