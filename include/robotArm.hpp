2f914a3cb2bf7a411ac1a2fdbcf0f670b63f9482
billalr <billalrahimi@hotmail.com>
2022-08-01 22:48:37 -0700

    changed transformation matrix function name

--- include/robotArm.hpp
+++ include/robotArm.hpp
@@ -30,7 +30,7 @@ class Arm{
         std::vector<std::vector<std::vector<float> > > armJointVec{ARM_JOINTS, std::vector<std::vector<float> >{POSE_M, std::vector<float>{POSE_N}}};
 
         // Function prototypes
-        std::vector<std::vector<float> > createLink(float rx, float ry, float rz, float tx, float ty, float tz);
+        std::vector<std::vector<float> > createHT(float tx, float ty, float tz, float rx, float ry, float rz);
         void update(std::vector<std::vector<std::vector<float> > > &joint);
         void homing(std::vector<std::vector<std::vector<float> > > &armJointVec);
         void printVector();
