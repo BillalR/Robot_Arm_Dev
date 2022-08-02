ed0f70b4a6663284303fce0fb4a8bea72433b21c
billalr <billalrahimi@hotmail.com>
2022-08-01 22:49:08 -0700

    Updated createLink function to createHT

--- src/robotArm.cpp
+++ src/robotArm.cpp
@@ -14,14 +14,15 @@ Arm::Arm()
 {
     // TODO --> Create an initializer that reads current servo values
     // Assign link initial values
-    armJointVec[0] = {createLink(0, 0, 0, 0, 0, 0)};
-    VecMult(armJointVec[0], createLink(0, 0, 200, 0, 0, 0), armJointVec[0]);
-    VecMult(armJointVec[1], createLink(0, 0, 200, 0, 0, 0), armJointVec[1]);
-    VecMult(armJointVec[2], createLink(0, 0, 200, 0, 0, 0), armJointVec[2]);
+    armJointVec[0] = {createHT(0, 0, 0, 0, 0, 0)};
+    VecMult(armJointVec[0], createHT(0, 0, 200, 0, 0, 0), armJointVec[1]);
+    VecMult(armJointVec[1], createHT(0, 0, 200, 0, 0, 0), armJointVec[2]);
+    VecMult(armJointVec[2], createHT(0, 0, 50, 0, 0, 0), armJointVec[3]);
 }
 
 void Arm::printVector()
 {
+    
     unsigned short int n = armJointVec.size();
     unsigned short int M = armJointVec[0].size();
     unsigned short int N = armJointVec[0][0].size();
@@ -42,7 +43,8 @@ void Arm::printVector()
         Serial.print(i + 1);
         Serial.println(" ");
         Serial.println(" ");
-    }
+    } 
+
 }
 
 /**
@@ -57,7 +59,7 @@ void Arm::update(std::vector<std::vector<std::vector<float> > > &joint)
     unsigned short int n = joint.size(); // Number of joints
     unsigned short int M = joint[0].size(); // Number of Rows
     unsigned short int N = joint[0][0].size(); // Number of Columns
-    
+
     // Peform link matrix multiplication
     /*
     for(unsigned short int i = 0; i < n - 1; i++)
@@ -94,17 +96,17 @@ void Arm::homing(std::vector<std::vector<std::vector<float> > > &armJointVec)
  * @param tz z translation (mm)
  * @return Link Vector
  */
-std::vector<std::vector<float> > Arm::createLink(float rx, float ry, float rz, float tx, float ty, float tz)
+std::vector<std::vector<float> > Arm::createHT(float tx, float ty, float tz, float rx, float ry, float rz)
 {
     // Multi-dimensional pose vector assignment
-    std::vector<std::vector<float> > Link = {
+    std::vector<std::vector<float> > T = {
         {cos(rz) * cos(ry), cos(rz) * sin(ry) * sin(rx) - sin(rz) * cos(rx), cos(rz) * sin(ry) * cos(rx) + sin(rz) * sin(rx), tx},
         {sin(rz) * cos(ry), sin(rz) * sin(ry) * sin(rx) + cos(rz) * cos(rx), sin(rz) * sin(ry) * cos(rx) - cos(rz) * sin(rx), ty},
         {(float)-1.0 * sin(ry), cos(ry) * sin(rx), cos(ry) * cos(rx), tz},
         {0, 0, 0, 1}
         };
 
-    return Link;
+    return T;
 }
 
 /**
