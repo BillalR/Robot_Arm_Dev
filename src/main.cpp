1a7fb4f61b750bf6ebdc4d03fc17c45129664bcf
billalr <billalrahimi@hotmail.com>
2022-08-05 12:10:04 -0700

    Implemented Servo class for testing purposes

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,40 @@
 
 // User defined hpp includes
 #include "robotArm.hpp"
+#include "ESP32Servo.hpp"
 
 // Defines
 #define BAUD_RATE 115200
+#define PWM_FREQ 50
+#define BASE_GPIO 25
+#define SHOULDER_GPIO 26
+#define ELBOW_GPIO 27
+
 
 // Class object inititalizer
 Arm robotArm;
+Servo Base;
+Servo Shoulder;
+Servo Elbow;
 
 void setup() {
   // Pin mode setups
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(BASE_GPIO, OUTPUT);
+  pinMode(SHOULDER_GPIO, OUTPUT);
+  pinMode(ELBOW_GPIO, OUTPUT);
+
+  //Configure servo objects
+  // Freqeuncy setup
+  Base.freqSetup(PWM_FREQ);
+  Shoulder.freqSetup(PWM_FREQ);
+  Elbow.freqSetup(PWM_FREQ);
+  //ledcSetup(0, 50, 8);
+  //ledcAttachPin(25, 0);
+  // Attach
+  Base.attach(BASE_GPIO);
+  Shoulder.attach(SHOULDER_GPIO);
+  Elbow.attach(ELBOW_GPIO);
 
   // Initialize Serial Comms
   Serial.begin(BAUD_RATE);
@@ -28,5 +52,20 @@ void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
-  robotArm.printVector();
+  //robotArm.printVector();
+
+  // Test Servos
+  for(int i = 0; i <= 180; i++)
+  {
+    Base.writePos(i);
+    delay(15);
+  }
+
+  for(int i = 180; i>=0; i--)
+  {
+    Base.writePos(i);
+    delay(15);
+  }
+
+  
 }
