1aa23ca01dc6bf5a9a5c960470f09ea796e6a0be
billalr <billalrahimi@hotmail.com>
2022-08-25 22:15:42 -0700

    unit conversions class file

---
+++ src/conversions.cpp
@@ -0,0 +1,16 @@
+/*
+Numerical units conversions
+
+Author: Billal Rahimi
+Email: billalrahimi@hotmail.com
+
+Date: 2022-07-29
+*/
+
+// Includes
+#include "conversions.hpp"
+
+float inches(int val)
+{
+    return val*25.4;
+}
