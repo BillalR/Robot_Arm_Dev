6f508d5848c523896e44311c0078b81cf233cde7
billalr <billalrahimi@hotmail.com>
2022-08-25 22:16:22 -0700

    adjusted class functions to better mimic real world application

--- src/robotArm.cpp
+++ src/robotArm.cpp
@@ -1,5 +1,5 @@
 /*
-Class functions used for robot arm manipulations
+Class functions used for robot arm manipulations.
 
 Author: Billal Rahimi
 Email: billalrahimi@hotmail.com
@@ -8,25 +8,52 @@ Date: 2022-07-29
 */
 
 #include "robotArm.hpp"
+#include "conversions.hpp"
 
 // Constructor
 Arm::Arm()
 {
     // TODO --> Create an initializer that reads current servo values
-    // TODO --> Better name for armJointVec, as it stores the HT vectors
+    // TODO --> Better name for transVec, as it stores the HT vectors
     // Assign link initial values
-    armJointVec[0] = {createHT(0, 0, 0, 0, 0, 0)};
-    VecMult(armJointVec[0], createHT(0, 0, 200, 0, 0, 0), armJointVec[1]);
-    VecMult(armJointVec[1], createHT(0, 0, 200, 0, 0, 0), armJointVec[2]);
-    VecMult(armJointVec[2], createHT(0, 0, 50, 0, 0, 0), armJointVec[3]);
+
+    // Assign transformation matrices initial values based on servo homing position
+    transVec[0] = {createHT(0, 0, 0, 0, 0, 180)};
+    VecMult(transVec[0], createHT(0, 0, inches(7), 0, 0, 180), transVec[1]);
+
+    // Assign Link physical length and rotation parameters
+    linkVec[0] = {{0}, {0}, {0}, {1}};
+    linkVec[1] = {{0}, {inches(7)}, {0}, {1}};
+
+
 }
 
-void Arm::printVector()
+/**
+ * Servo move routine based on the robot arm link set pose
+ * 
+ * @param transVec pass the current vector set of pose transformations
+ * @return Void
+ */
+
+void Arm::move(float tx, float ty, float tz)
+{
+    // Update 
+    // Assign transformation matrices initial values based on servo homing position
+    transVec[0] = {createHT(0, 0, 0, 0, 0, 180)};
+    VecMult(transVec[0], createHT(inches(tx), inches(ty), inches(tz), 0, 0, 180), transVec[1]);
+
+    Serial.println("added values");
+    update();
+
+    return;
+}
+
+void Arm::printTransVector()
 {
     
-    unsigned short int n = armJointVec.size();
-    unsigned short int M = armJointVec[0].size();
-    unsigned short int N = armJointVec[0][0].size();
+    unsigned short int n = transVec.size();
+    unsigned short int M = transVec[0].size();
+    unsigned short int N = transVec[0][0].size();
 
     for(unsigned short int i = 0; i < n; i++)
     {
@@ -34,7 +61,34 @@ void Arm::printVector()
         {
             for(unsigned short int k = 0; k < N; k++)
             {
-                Serial.print(armJointVec[i][j][k]);
+                Serial.print(transVec[i][j][k]);
+                Serial.print("    ");
+            }
+            Serial.println(" ");
+        }
+        Serial.println(" ");
+        Serial.print("End of Joint ");
+        Serial.print(i + 1);
+        Serial.println(" ");
+        Serial.println(" ");
+    } 
+
+}
+
+void Arm::printLinkVector()
+{
+    
+    unsigned short int n = linkVec.size();
+    unsigned short int M = linkVec[0].size();
+    unsigned short int N = linkVec[0][0].size();
+
+    for(unsigned short int i = 0; i < n; i++)
+    {
+        for(unsigned short int j = 0; j < M; j++)
+        {
+            for(unsigned short int k = 0; k < N; k++)
+            {
+                Serial.print(linkVec[i][j][k]);
                 Serial.print("    ");
             }
             Serial.println(" ");
@@ -54,29 +108,37 @@ void Arm::printVector()
  * @param joint pass the current vector set of pose transformations (3D)
  * @return Void
  */
-void Arm::update(std::vector<std::vector<std::vector<float> > > &joint)
+void Arm::update()//std::vector<std::vector<std::vector<float> > > &joint)
 {
 
-    unsigned short int n = joint.size(); // Number of joints
-    unsigned short int M = joint[0].size(); // Number of Rows
-    unsigned short int N = joint[0][0].size(); // Number of Columns
+    unsigned short int nTrans = transVec.size(); // Number of transformations
+    unsigned short int MTrans = transVec[0].size(); // Number of Transformation Rows
 
-    // Peform link matrix multiplication
-    /*
-    for(unsigned short int i = 0; i < n - 1; i++)
+    unsigned short int nLink = linkVec.size(); // Number of links
+    unsigned short int MLink = linkVec[0].size(); // Number of Link Rows
+
+    // Peform transformation vector multiplication
+    for(unsigned short int i = 0; i < nTrans - 1; i++)
     {
-        VecMult(joint[i], joint[i + 1], joint[i + 1]);
+        // Update the transformation matrices
+        VecMult(transVec[i], transVec[i + 1], transVec[i + 1]);
     }
-    */
+
+    // Peform transformation multiplication on physical link vectors
+    for(unsigned short int i = 0; i < nTrans; i++)
+    {
+        // Update the physical links based on transformation calcs above
+        VecMult(linkVec[i], transVec[i], linkVec[i]);
+    }
 }
 
 /**
  * Robot homing routine --> Robot arm is upright in the vertical direction
  * 
- * @param armJointVec pass the current vector set of pose transformations (3D)
+ * @param transVec pass the current vector set of pose transformations (3D)
  * @return Void
  */
-void Arm::homing(std::vector<std::vector<std::vector<float> > > &armJointVec)
+void Arm::homing(std::vector<std::vector<std::vector<float> > > &transVec)
 {
     return;
 }
@@ -103,25 +165,12 @@ std::vector<std::vector<float> > Arm::createHT(float tx, float ty, float tz, flo
     std::vector<std::vector<float> > T = {
         {cos(rz) * cos(ry), cos(rz) * sin(ry) * sin(rx) - sin(rz) * cos(rx), cos(rz) * sin(ry) * cos(rx) + sin(rz) * sin(rx), tx},
         {sin(rz) * cos(ry), sin(rz) * sin(ry) * sin(rx) + cos(rz) * cos(rx), sin(rz) * sin(ry) * cos(rx) - cos(rz) * sin(rx), ty},
-        {(float)-1.0 * sin(ry), cos(ry) * sin(rx), cos(ry) * cos(rx), tz},
+        {(float)0 * sin(ry), cos(ry) * sin(rx), cos(ry) * cos(rx), tz},
         {0, 0, 0, 1}
         };
 
     return T;
 }
-
-/**
- * Servo move routine based on the robot arm link set pose
- * 
- * @param armJointVec pass the current vector set of pose transformations
- * @return Void
- */
-
-void Arm::moveServo(std::vector<std::vector<std::vector<float> > > &armJointVec)
-{
-    return;
-}
-
 
 // Deconstructor
 Arm::~Arm()
