2399de152508bc7dde41606d2d2ff52b49eb0d32
billalr <billalrahimi@hotmail.com>
2022-08-25 22:15:06 -0700

    updated function definitions

--- include/robotArm.hpp
+++ include/robotArm.hpp
@@ -4,9 +4,11 @@
 #include <math.h>
 #include "vectorMath.hpp"
 // Defines
-#define ARM_JOINTS 4
+#define ARM_JOINTS 2
 #define POSE_M 4
 #define POSE_N 4
+#define LINK_M 4
+#define LINK_N 1
 
 /**
  * Robot Arm Class
@@ -18,6 +20,7 @@
 class Arm{
     // Class private params
     private:
+        // Variables Definitions
 
     // Class public params
     public:
@@ -27,14 +30,15 @@ class Arm{
         ~Arm();
 
         // Variable definitions
-        std::vector<std::vector<std::vector<float> > > armJointVec{ARM_JOINTS, std::vector<std::vector<float> >{POSE_M, std::vector<float>{POSE_N}}};
-
+        std::vector<std::vector<std::vector<float> > > transVec{ARM_JOINTS, std::vector<std::vector<float> >{POSE_M, std::vector<float>{POSE_N}}};
+        std::vector<std::vector<std::vector<float> > > linkVec{ARM_JOINTS, std::vector<std::vector<float> >{LINK_M, std::vector<float>{LINK_N}}};
         // Function prototypes
         std::vector<std::vector<float> > createHT(float tx, float ty, float tz, float rx, float ry, float rz);
-        void update(std::vector<std::vector<std::vector<float> > > &joint);
+        void update();//std::vector<std::vector<std::vector<float> > > &joint);
         void homing(std::vector<std::vector<std::vector<float> > > &armJointVec);
-        void printVector();
-        void moveServo(std::vector<std::vector<std::vector<float>>> &armJointVec);
+        void printTransVector();
+        void printLinkVector();
+        void move(float tx, float ty, float tz);
 };
 
 
