0305306f74da40db691a522d7396a19bdac3fe1b
billalr <billalrahimi@hotmail.com>
2022-08-25 22:15:58 -0700

    testing servo functions

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,8 @@
 
 // User defined hpp includes
 #include "robotArm.hpp"
-#include "ESP32Servo.hpp"
+#include "Servo.hpp"
+#include "conversions.hpp"
 
 // Defines
 #define BAUD_RATE 115200
@@ -18,12 +19,14 @@
 
 
 // Class object inititalizer
-Arm robotArm;
+Arm robot;
 Servo Base;
 Servo Shoulder;
 Servo Elbow;
 
+bool INIT = true;
 void setup() {
+  
   // Pin mode setups
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(BASE_GPIO, OUTPUT);
@@ -35,12 +38,16 @@ void setup() {
   Base.freqSetup(PWM_FREQ);
   Shoulder.freqSetup(PWM_FREQ);
   Elbow.freqSetup(PWM_FREQ);
-  //ledcSetup(0, 50, 8);
-  //ledcAttachPin(25, 0);
+
   // Attach
   Base.attach(BASE_GPIO);
   Shoulder.attach(SHOULDER_GPIO);
   Elbow.attach(ELBOW_GPIO);
+
+  // Initialize Servo Homing
+  Base.homeServo();
+  Shoulder.homeServo();
+  Elbow.homeServo();
 
   // Initialize Serial Comms
   Serial.begin(BAUD_RATE);
@@ -48,36 +55,75 @@ void setup() {
 }
 
 void loop() {
-  delay(1000);
+  delay(500);
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
+  delay(500);
   digitalWrite(LED_BUILTIN, LOW);
   //robotArm.printVector();
 
+  Serial.println("Transformation Vector");
+  robot.printTransVector();
+
+  Serial.println("Link Vector");
+  robot.printLinkVector();
+
+  //delay(2000);
+  //if(INIT)
+  //{
+    //robot.move(2, 0, 0);
+    //INIT = false;
+  //}
+  //robotArm.update();
   // Test Servos
-  for(int i = 0; i <= 180; i++)
+  //Base.writePos(107);
+  //Shoulder.writePos(168);
+
+  delay(2000);
+  if(INIT)
   {
-    Base.writePos(i);
-    delay(15);
+  robot.move(0, 2, 0);
+  INIT = false;
   }
 
-  for(int i = 0; i <= 180; i++)
-  {
-    Shoulder.writePos(i);
-    delay(15);
-  }
+  // for(unsigned short int i = 0; i < (unsigned short int)robot.linkVec[0][3][0]; i++)
+  // {
+  //   Base.writePos(i);
+  //   delay(25);
+  // }
 
-  for(int i = 180; i>=0; i--)
-  {
-    Base.writePos(i);
-    delay(15);
-  }
+  // for(unsigned short int i = 0; i < (unsigned short int)robot.linkVec[1][3][0]; i++)
+  // {
+  //   Shoulder.writePos(i);
+  //   delay(25);
+  // }
 
-  for(int i = 180; i>=0; i--)
-  {
-    Shoulder.writePos(i);
-    delay(15);
-  }
+  // INIT = false;
+  // }
+  
+
+  // for(int i = 0; i <= 180; i++)
+  // {
+  //   Base.writePos(i);
+  //   delay(15);
+  // }
+
+  // for(int i = 0; i <= 180; i++)
+  // {
+  //   Shoulder.writePos(i);
+  //   delay(15);
+  // }
+
+  // for(int i = 180; i>=0; i--)
+  // {
+  //   Base.writePos(i);
+  //   delay(15);
+  // }
+
+  // for(int i = 180; i>=0; i--)
+  // {
+  //   Shoulder.writePos(i);
+  //   delay(15);
+  // }
 
   
 }
