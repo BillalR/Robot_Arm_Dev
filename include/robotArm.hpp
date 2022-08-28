54e525977c54c168a800bd18d806fda8ce3f3c13
billalr <billalrahimi@hotmail.com>
2022-08-28 00:13:16 -0700

    updated the number of joints definition

--- include/robotArm.hpp
+++ include/robotArm.hpp
@@ -4,7 +4,7 @@
 #include <math.h>
 #include "vectorMath.hpp"
 // Defines
-#define ARM_JOINTS 4
+#define ARM_JOINTS 3
 #define POSE_M 4
 #define POSE_N 4
 #define LINK_M 4
@@ -32,6 +32,7 @@ class Arm{
         // Variable definitions
         std::vector<std::vector<std::vector<float> > > transVec{ARM_JOINTS, std::vector<std::vector<float> >{POSE_M, std::vector<float>{POSE_N}}};
         std::vector<std::vector<std::vector<float> > > linkVec{ARM_JOINTS, std::vector<std::vector<float> >{LINK_M, std::vector<float>{LINK_N}}};
+        
         // Function prototypes
         std::vector<std::vector<float> > createHT(float tx, float ty, float tz, float rx, float ry, float rz);
         void update();//std::vector<std::vector<std::vector<float> > > &joint);
