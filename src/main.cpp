92f67bd5fe2ecc464fa6a7ee0bbe5bcf614b8aef
billalr <billalrahimi@hotmail.com>
2022-08-28 00:13:47 -0700

    added ikine test functions for robot arm 3 link

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,8 @@
 #define BASE_GPIO 25
 #define SHOULDER_GPIO 26
 #define ELBOW_GPIO 27
-
+#define PI 3.1415926
+#define LINK_LENGTH 177.8 // Length in mm
 
 // Class object inititalizer
 Arm robot;
@@ -24,7 +25,12 @@ Servo Base;
 Servo Shoulder;
 Servo Elbow;
 
-bool INIT = true;
+// Function prototypes
+void move_angle(Servo &joint, int rot);
+bool move_robot(int x, int y, int z);
+double r2d(double theta);
+void home_servos();
+
 void setup() {
   
   // Pin mode setups
@@ -45,9 +51,12 @@ void setup() {
   Elbow.attach(ELBOW_GPIO);
 
   // Initialize Servo Homing
-  Base.homeServo();
-  Shoulder.homeServo();
-  Elbow.homeServo();
+  // Base.homeServo();
+  // Shoulder.homeServo();
+  // Elbow.homeServo();
+
+  // Home Servos
+  home_servos();
 
   // Initialize Serial Comms
   Serial.begin(BAUD_RATE);
@@ -59,71 +68,106 @@ void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
   digitalWrite(LED_BUILTIN, LOW);
-  //robotArm.printVector();
 
-  Serial.println("Transformation Vector");
-  robot.printTransVector();
+  move_robot(150, 150, 250);
+  delay(250);
+  move_robot(50, 100, 100);
+  delay(250);
+  move_robot(25, 200, 50);
 
-  Serial.println("Link Vector");
-  robot.printLinkVector();
+}
 
-  //delay(2000);
-  //if(INIT)
-  //{
-    //robot.move(2, 0, 0);
-    //INIT = false;
-  //}
-  //robotArm.update();
-  // Test Servos
-  //Base.writePos(107);
-  //Shoulder.writePos(168);
+/**
+ * Move the servos at a smoother pace -- TEST
+ * 
+ * @param &joint Servo object reference used to rotate the specific joint -- MUST BE REFERENCE OR ELSE CLASS DOESN'T UPDATE THIS POINTER
+ * @param rot rotation in degrees
+ * @return void
+ */
+void move_angle(Servo &joint, int rot)
+{
+  int current_pos = joint.getCurrentPos();
 
-  delay(2000);
-  if(INIT)
+  if(current_pos < rot)
   {
-  robot.move(0, 2, 0);
-  INIT = false;
+    for(int i = current_pos; i <= rot; i++)
+    {
+    joint.writePos(i);
+    delay(25);
+    }
+  }else
+  {
+    for(int i = current_pos; i >= rot; i--)
+    {
+    joint.writePos(i);
+    delay(25);
+    }
   }
-
-  // for(unsigned short int i = 0; i < (unsigned short int)robot.linkVec[0][3][0]; i++)
-  // {
-  //   Base.writePos(i);
-  //   delay(25);
-  // }
-
-  // for(unsigned short int i = 0; i < (unsigned short int)robot.linkVec[1][3][0]; i++)
-  // {
-  //   Shoulder.writePos(i);
-  //   delay(25);
-  // }
-
-  // INIT = false;
-  // }
   
+}
 
-  // for(int i = 0; i <= 180; i++)
-  // {
-  //   Base.writePos(i);
-  //   delay(15);
-  // }
+/**
+ * Initialize Servos in their home orientaiton
+ * 
+ * @return void
+ */
+void home_servos()
+{
+  move_angle(Base, 0);
+  move_angle(Shoulder, 180);
+  move_angle(Elbow, 0);
+}
 
-  // for(int i = 0; i <= 180; i++)
-  // {
-  //   Shoulder.writePos(i);
-  //   delay(15);
-  // }
+/**
+ * Servo move routine based on the robot arm servo rotations -- TEST
+ * 
+ * @param x x-position
+ * @param y y-position
+ * @param z z-position
+ * @return bool Whether the position was executed or if there is a singularity
+ */
+bool move_robot(int x, int y, int z)
+{
+  // Calculate the base joint rotation based on the x and y position
+  double base_rotation = r2d(atan(y/x));
+  Serial.println(base_rotation);
 
-  // for(int i = 180; i>=0; i--)
-  // {
-  //   Base.writePos(i);
-  //   delay(15);
-  // }
+  Serial.println("base before degree conversion");
+  Serial.println(atan(y/x));
 
-  // for(int i = 180; i>=0; i--)
-  // {
-  //   Shoulder.writePos(i);
-  //   delay(15);
-  // }
+  // Rotate the base joint
+  move_angle(Base, (int)base_rotation);
 
-  
+  // Length of position from base in 2D coordinate frame -- (x,y)
+  float length_2D = sqrt(pow(x, 2) + pow(y, 2));
+
+  // Length of position from base in z 2D coordinate frame -- (length_2D, z)
+  float length_3D = sqrt(pow(length_2D, 2) + pow(z, 2));
+
+  float phi = atan(z/length_2D);
+
+  // This is a singularity region
+  if (length_3D > LINK_LENGTH*2) { return false;}
+
+  float theta = acos((length_3D/2)/(float)LINK_LENGTH);
+
+  float a1 = 180 - r2d(phi - theta);
+  float a2 = r2d(phi + theta);
+
+  move_angle(Shoulder, (int)a1);
+  move_angle(Elbow, (int)a2);
+
+
+  return true;
+}
+
+/**
+ * Servo move routine based on the robot arm servo rotations -- TEST
+ * 
+ * @param theta angle value in radians
+ * @return float angle in degrees
+ */
+double r2d(double theta)
+{
+  return theta*180/PI;
 }
